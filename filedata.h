99217daf1e62f17bb761159dd63dac075309b80e
ian <muted_muscle_twig@hotmail.com>
2011-03-28 14:04:23 -0700

    implemented read and write in filedata

--- filedata.h
+++ filedata.h
@@ -2,13 +2,33 @@
 #define FILEDATA_H
 
 #include <QObject>
+#include <QFile>
 
 class filedata : public QObject
 {
     Q_OBJECT
+private:
+    QString name_;
+    QByteArray data_;
+
 public:
     explicit filedata(QObject *parent = 0);
+    explicit filedata(QObject *parent, QString name, QByteArray data);
+    void setName(QString name) {
+        name_ = name;
+    }
+    QString getName() {
+        return name_;
+    }
+    void setData(QByteArray data) {
+        data_ = data;
+    }
+    QByteArray getData() {
+        return data_;
+    }
 
+    bool writeToFile();
+    bool readFromFile();
 signals:
 
 public slots:
