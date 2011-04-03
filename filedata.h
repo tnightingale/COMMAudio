e9a7b3d2d3599b295145c01610a737f5ce092212
Luke Queenan <lukequeenan@gmail.com>
2011-04-02 21:30:04 -0700

    Added a length variable for the current transfer class. This should be filled out using the first 8 bytes of a packet.

--- filedata.h
+++ filedata.h
@@ -14,29 +14,44 @@ private:
     QString path_;
     QByteArray data_;
     short port_;
+    int totalSize_;
 
 public:
     explicit FileData(QObject *parent = 0);
     explicit FileData(QObject *parent, QString path, short port);
     explicit FileData(QObject *parent, short port);
     explicit FileData(QObject *parent, QString path, QByteArray data, short port);
+
     void setpath(QString path) {
         path_ = path;
     }
+
     QString getPath() {
         return path_;
     }
+
     void setData(QByteArray data) {
         data_ = data;
     }
+
     QByteArray getData() {
         return data_;
     }
+
     void setPort(short port) {
         port_ = port;
     }
+
     short getPort() {
         return port_;
+    }
+
+    void setTotalSize(int size) {
+        totalSize_ = size;
+    }
+
+    int getTotalSize() {
+        return totalSize_;
     }
 
     void append(QByteArray data) {
