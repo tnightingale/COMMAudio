426ea1d115e76b94b639f0846e2ef326cc456b90
Tom Nightingale <tom@tnightingale.com>
2011-04-05 11:01:17 -0700

    audio output and input are now back on the regular thread

--- audiocomponent.cpp
+++ audiocomponent.cpp
@@ -124,7 +124,7 @@ void AudioComponent::startMic(QIODevice* stream, QThread* socketThread) {
     input_ = new QAudioInput(format, NULL);
     connect(input_, SIGNAL(stateChanged(QAudio::State)),
             this, SLOT(mic(QAudio::State)));
-    input_->moveToThread(socketThread);
+    //input_->moveToThread(socketThread);
     input_->start(stream);
 }
 
@@ -162,7 +162,7 @@ void AudioComponent::playStream(QIODevice* stream, QThread* socketThread){
     output_ = new QAudioOutput(format,NULL);
     connect(output_, SIGNAL(stateChanged(QAudio::State)),
             this, SLOT(speak(QAudio::State)));
-    output_->moveToThread(socketThread);
+    //output_->moveToThread(socketThread);
     output_->start(stream);
 }
 
